81276705b5a5320a092e9d6752c1d4b003ce9138
Kai Xu <xukai921110@gmail.com>
2016-04-27 00:10:46 +0100

    make csv output and start sfotmax

--- exp/logistic.py
+++ exp/logistic.py
@@ -14,7 +14,7 @@ import flymc as ff
 # Set hyperparameters
 stepsize = 0.02          # size of Metropolis-Hastings step in theta
 th0 = 1.5               # scale of weights
-N_steps = 3000
+N_steps = 5000
 N_ess = 2000
 
 def preprocess():
@@ -50,27 +50,23 @@ def main():
         th_lists = []           # trace of th
         num_rejects_list = []   #
         num_iter_list = []      # number of num_lik_evals for each iteration
+        neg_log_post_list = []  # neg log posterior
         for _ in range(N_steps):
             num_lik_prev = model.num_lik_evals
-            if _ % N_ess == 0 and _ > 0:
-                #print pypmc.tools.convergence.ess(th_lists) # TODO: is this correct?
-                #print ess(th_lists)
-                np.savetxt('trace-untuned-{0}.csv'.format(_), np.array(th_lists))
-                th_lists = []
             th = th_stepper.step(th, z)  # Markov transition step for theta
-
-
             if fly:
                 z  = z__stepper.step(th ,z)  # Markov transition step for z
             th_lists.append(th)
             num_rejects_list.append(th_stepper.num_rejects)
             num_iter_list.append(model.num_lik_evals - num_lik_prev)
+            neg_log_post_list.append(-1.0 * model.log_p_marg(th, increment_ctr=False))
             print "Accept rate: {0}".format(1.0 - sum(num_rejects_list)/float(_+1))
-            print "Likelihood evals in iter {0}: {1}".format(_, model.num_lik_evals - num_lik_prev)
-            print "Neg log posterior: {0}".format(-1.0 * model.log_p_marg(th, increment_ctr=False))
+            print "Likelihood evals in iter {0}: {1}".format(_, num_iter_list[-1])
+            print "Neg log posterior: {0}".format(neg_log_post_list[-1])
             print "Number bright points: {0}".format(len(z.bright))
 
-        return th, num_iter_list, th_lists
+        return num_iter_list, th_lists, neg_log_post_list
+
     # preprocess data and save to .npy file
     # so tha we dont have to PCA them each time
     # preprocess()
@@ -81,51 +77,30 @@ def main():
     N, D = x.shape
     y0 = 1.5 # \xce in paper
 
-
-    def ess(th_list):
-        th = np.array(th_list)
-        th_mean = np.mean(th, axis=0)
-        def autocorr(x, t):
-            return np.mean((x[0:len(x)-t,:] - th_mean) * (x[t:len(x),:] - th_mean))
-        return 1.0 * th.shape[0] / (1.0 + 2.0 * sum(map(lambda t: autocorr(th,t), range(1,th.shape[0]))))
-
-    # print ess([
-    #     np.array([1]),
-    #     np.array([1.1]),
-    #     np.array([0.9]),
-    #     np.array([1])
-    #     ])
-
-
-    # model_mcmc = ff.LogisticModel(x, t, th0=th0, y0=y0)
-    # print model_mcmc.num_lik_evals
-    # th_mcmc, num_iter_list_mcmc, th_lists_mcmc = run_model(model_mcmc)
-    # print model_mcmc.num_lik_evals
-
+    model_mcmc = ff.LogisticModel(x, t, th0=th0, y0=y0)
+    num_iter_mcmc, th_mcmc, neg_log_post_mcmc = run_model(model_mcmc)
 
     model_flymc = ff.LogisticModel(x, t, th0=th0, y0=y0)
-    # print model_flymc.num_lik_evals
-    th_flymc, num_iter_list_flymc, th_lists_flymc = run_model(model_flymc, q=0.1, fly=True) # q = prob(dim -> bright)
-    # print model_flymc.num_lik_evals
+    num_iter_flymc, th_flymc, neg_log_post_flymc = run_model(model_flymc, q=0.1, fly=True) # q = prob(dim -> bright)
 
-
-
-    #_model = ff.LogisticModel(x, t, th0=th0)
-    #th = np.random.randn(D) * th0
-    #th_map = optimize.minimize(lambda x: -1*_model.log_p_marg(x), th)
-    #model_flymc_map = ff.LogisticModel(x, t, th0=th0, th_map=th_map.x)
-    #print run_model(model_flymc_map, q=0.01, fly=True)
-    #print model_flymc_map.num_lik_evals
+    # _model = ff.LogisticModel(x, t, th0=th0)    # dummy model used to optimize th
+    # th = np.random.randn(D) * th0
+    # th_map = optimize.minimize(lambda x: -1*_model.log_p_marg(x), th)
+    # np.save('th_map.npy', th_map.x)
+    th_map = np.load('th_map.npy')
+    model_flymc_map = ff.LogisticModel(x, t, th0=th0, th_map=th_map)
+    num_iter_flymc_map, th_flymc_map, neg_log_post_flymc_map = run_model(model_flymc_map, q=0.01, fly=True)
 
     # output traces to .csv
-    # print num_iter_list_flymc
-    # with open('num_iter_list_mcmc', 'wb') as f:
-    #     writer = csv.writer(f)
-    #     writer.writerows(num_iter_list_flymc)
-
-    # plt.plot(num_iter_list_mcmc)
-    plt.plot(num_iter_list_flymc)
-    plt.show()
+    np.savetxt('logistic_num_iter_mcmc.csv', np.array(num_iter_mcmc))
+    np.savetxt('logistic_th_mcmc.csv', np.array(th_mcmc))
+    np.savetxt('logistic_neg_log_post_mcmc.csv', np.array(neg_log_post_mcmc))
+    np.savetxt('logistic_num_iter_flymc.csv', np.array(num_iter_flymc))
+    np.savetxt('logistic_th_flymc.csv', np.array(th_flymc))
+    np.savetxt('logistic_neg_log_post_flymc.csv', np.array(neg_log_post_flymc))
+    np.savetxt('logistic_num_iter_flymc_map.csv', np.array(num_iter_flymc_map))
+    np.savetxt('logistic_th_flymc_map.csv', np.array(th_flymc_map))
+    np.savetxt('logistic_neg_log_post_flymc_map.csv', np.array(neg_log_post_flymc_map))
 
 if __name__ == "__main__":
     main()
