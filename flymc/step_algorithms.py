b957a4e13bbc27920b9a76168e5dd0b9f85225a1
Feynman Liang <feynman.liang@gmail.com>
2016-04-26 19:55:12 +0100

    Replaces with our own cache code

--- flymc/step_algorithms.py
+++ flymc/step_algorithms.py
@@ -105,12 +105,12 @@ class zStepMH(Stepper):
         Nd = proposed_darken.size
 
         # Consider the bright -> dark proposals
-        log_p_accept = np.log(self.q) - self.log_bd_ratio(th, proposed_darken, increment_ctr=False)
+        log_p_accept = np.log(self.q) - self.log_bd_ratio(th, proposed_darken)
         idxs_accepted = proposed_darken[np.log(npr.rand(Nd)) <  log_p_accept]
         z.darken(idxs_accepted)
 
         # Consider the dark -> bright proposals
-        log_p_accept = -np.log(self.q) + self.log_bd_ratio(th, proposed_brighten, increment_ctr=False)
+        log_p_accept = -np.log(self.q) + self.log_bd_ratio(th, proposed_brighten)
         idxs_accepted = proposed_brighten[np.log(npr.rand(Nb)) <  log_p_accept]
         z.brighten(idxs_accepted)
 
