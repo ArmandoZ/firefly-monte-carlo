59622e14f592e1c37cf09abd89c79a81c8c2246c
zhaoguo zhu <>
2019-03-18 12:38:10 +0800

    Add effective sample size tracing

--- experiment/logistic.py
+++ experiment/logistic.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pypmc
 import csv
 import sys
+import os
 sys.path.append('..')
 import flymc as ff
 import data
@@ -17,10 +18,13 @@ N_steps = 3000
 N_ess = 2000
 
 # Performance keys
+KEY_NAME = "KEY_NAME"
 KEY_NUM_REJECTS = "KEY_NUM_REJECTS"
 KEY_ACCEPTANCE = "KEY_ACCEPTANCE"
 KEY_LIK_EVALS = "KEY_LIK_EVALS"
 KEY_NEG_LOG = "KEY_NEG_LOG"
+KEY_SAMPLE_EVALS = "KEY_SAMPLE_EVALS"
+KEY_SAMPLE_ESS = "KEY_SAMPLE_ESS"
 
 def run_model(model, q=0.1, fly=False, verbose=False):
     '''
@@ -38,27 +42,22 @@ def run_model(model, q=0.1, fly=False, verbose=False):
         z_stepper = ff.zStepMH(model.log_pseudo_lik, q)
 
     # Trace list
-    th_list = []
     num_rejects_list = []
     acceptance_list = []
     num_lik_evals_list = []
     neg_log_list = []
     performance_dict = {}
+    # Trace - Sampling
+    sample_num_lik_evals_list = []
+    sample_th_list = []
 
     # Run chain
-    for _ in range(N_steps):
+    for _ in range(N_steps + N_ess):
         num_lik_prev = model.num_lik_evals
-        if _ % N_ess == 0 and _ > 0:
-            #print pypmc.tools.convergence.ess(th_list) # TODO: is this correct?
-            #print ess(th_list)
-            np.savetxt('trace-{0}-{1}.csv'.format(model.name, _), np.array(th_list))
-            th_list = []
-
         # Markov transition
         th = th_stepper.step(th, z)  # Markov transition step for theta
         if fly:
             z = z_stepper.step(th, z)  # Markov transition step for z
-        th_list.append(th)
 
         # Record performance
         num_rejects = th_stepper.num_rejects
@@ -66,10 +65,15 @@ def run_model(model, q=0.1, fly=False, verbose=False):
         acceptance = 1.0 - sum(num_rejects_list)/float(_+1)
         neg_log = -1.0 * model.log_p_marg(th, increment_ctr=False)
 
-        num_rejects_list.append(num_rejects)
-        num_lik_evals_list.append(num_lik_evals)
-        acceptance_list.append(acceptance)
-        neg_log_list.append(neg_log)
+        if _ < N_steps:
+            num_rejects_list.append(num_rejects)
+            num_lik_evals_list.append(num_lik_evals)
+            acceptance_list.append(acceptance)
+            neg_log_list.append(neg_log)
+        else: 
+            # _ >= N_steps
+            sample_num_lik_evals_list.append(num_lik_evals)
+            sample_th_list.append(th)
  
         # Print info
         if verbose or (_ % 50 == 0):
@@ -78,13 +82,22 @@ def run_model(model, q=0.1, fly=False, verbose=False):
             print "Neg log posterior: {0}".format(neg_log)
             print "Number bright points: {0}".format(len(z.bright))
 
+    sample_ess = ess(sample_th_list)
+    sample_evals = sum(sample_num_lik_evals_list)
+    np.savetxt('trace-{0}-{1}.csv'.format(model.name, sample_ess), np.array(sample_th_list))
+
+    performance_dict[KEY_NAME] = model.name
     performance_dict[KEY_NUM_REJECTS] = num_rejects_list
     performance_dict[KEY_LIK_EVALS] = num_lik_evals_list
     performance_dict[KEY_ACCEPTANCE] = acceptance_list
     performance_dict[KEY_NEG_LOG] = neg_log_list
+    performance_dict[KEY_SAMPLE_EVALS] = sample_evals
+    performance_dict[KEY_SAMPLE_ESS] = sample_ess
+
     return performance_dict
 
 def ess(th_list):
+    # Alternate: pypmc.tools.convergence.ess(th_list) # TODO: is this correct?
     th = np.array(th_list)
     th_mean = np.mean(th, axis=0)
 
@@ -92,6 +105,40 @@ def ess(th_list):
         return np.mean((x[0:len(x)-t,:] - th_mean) * (x[t:len(x),:] - th_mean))
 
     return 1.0 * th.shape[0] / (1.0 + 2.0 * sum(map(lambda t: autocorr(th,t), range(1,th.shape[0]))))
+
+def save_results(performance_results):
+    ## Plot
+    # Plot negative log posteriors
+    if not os.path.exists("./result"):
+        os.mkdir("./result")
+
+    plt.cla()
+    for performance_result in performance_results:
+        y = performance_result[KEY_NEG_LOG]
+        label = performance_result[KEY_NAME]
+        plt.plot(range(len(y)), y, label=label)
+    plt.title("Negative Log Posteriors")
+    plt.legend(loc='best')
+    plt.savefig("./result/%s.png" % "Negative Log Posteriors")
+
+    # Plot Mean Likelihood Evaluations
+    plt.cla()
+    for performance_result in performance_results:
+        y = performance_result[KEY_LIK_EVALS]
+        label = performance_result[KEY_NAME]
+        plt.plot(range(len(y)), y, label=label)
+    plt.title("Mean Likelihood Evaluations")
+    plt.legend(loc='best')
+    plt.savefig("./result/%s.png" % "Mean Likelihood Evaluations")
+
+    ## Sample
+    with open("./result/sample_result.csv", 'w') as f:
+        f.write("model_name,ESS,mean_likelihood_evaluation,product\n")
+        for performance_result in performance_results:
+            model_name = performance_result[KEY_NAME]
+            sample_ess = performance_result[KEY_SAMPLE_ESS]
+            sample_evals = performance_result[KEY_SAMPLE_EVALS]
+            f.write("{0},{1},{2},{3}\n" % (model_name, sample_ess, sample_evals, sample_ess * sample_evals))
 
 def main():
     global N
@@ -102,33 +149,32 @@ def main():
     N, D = x.shape
     y0 = 1.5 # \xce in paper
 
-    # model_mcmc = ff.LogisticModel(x, t, th0=th0, y0=y0)
-    # print model_mcmc.num_lik_evals
-    # th_mcmc, num_lik_evals_list_mcmc, th_list_mcmc = run_model(model_mcmc)
-    # print model_mcmc.num_lik_evals
+    performance_results = []
+
+    model_mcmc = ff.LogisticModel(x, t, th0=th0, y0=y0, name="regular mcmc")
+    mcmc_performance = run_model(model_mcmc)
+    performance_results.append(mcmc_performance)
 
     model_untuned_flymc = ff.LogisticModel(x, t, th0=th0, y0=y0, name="untuned_flymc")
-    # print model_flymc.num_lik_evals
     untuned_flymc_performance = run_model(model_untuned_flymc, q=0.1, fly=True) # q = prob(dim -> bright)
-    # print model_flymc.num_lik_evals
+    performance_results.append(untuned_flymc_performance)
 
-    #_model = ff.LogisticModel(x, t, th0=th0)
-    #th = np.random.randn(D) * th0
-    #th_map = optimize.minimize(lambda x: -1*_model.log_p_marg(x), th)
-    #model_flymc_map = ff.LogisticModel(x, t, th0=th0, th_map=th_map.x)
-    #print run_model(model_flymc_map, q=0.01, fly=True)
-    #print model_flymc_map.num_lik_evals
+    tmp_model = ff.LogisticModel(x, t, th0=th0)
+    th_map = optimize.minimize(
+            fun=lambda th: -1.0*tmp_model.log_p_marg(th),
+            x0=np.random.randn(D)*th0,
+            jac=lambda th: -1.0*tmp_model.D_log_p_marg(th),
+            method='BFGS',
+            options={
+                'maxiter': 100,
+                'disp': True
+            })
 
-    # output traces to .csv
-    # print num_lik_evals_list_flymc
-    # with open('num_lik_evals_list_mcmc', 'wb') as f:
-    #     writer = csv.writer(f)
-    #     writer.writerows(num_lik_evals_list_flymc)
+    model_tuned_flymc = ff.LogisticModel(x, t, th0=th0, th_map=th_map.x, name="tuned_flymc")
+    tuned_flymc_performance = run_model(model_tuned_flymc, q=0.01, fly=True)
+    performance_results.append(tuned_flymc_performance)
 
-    # plt.plot(num_lik_evals_list_mcmc)
-    num_lik_evals_list_flymc = untuned_flymc_performance[KEY_LIK_EVALS]
-    plt.plot(num_lik_evals_list_flymc)
-    plt.show()
+    save_results(performance_results)
 
 if __name__ == "__main__":
     main()
